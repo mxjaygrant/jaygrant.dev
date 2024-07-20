b0890975cc017d8e6ab7615252211bd88cd4a467
Jay Grant <dev@jaygrant.dev>
2024-07-20 02:53:08 -0400

    clean up display of color generation, make aside sticky

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 let colorPicker;
-const defaultColor = "#FB2AEB";
+const defaultColor = "#FB2AC7";    //color picker only tolerates hex, no I don't know why it keeps defaulting to a purple
 
 window.addEventListener("load", startup, false);
 
