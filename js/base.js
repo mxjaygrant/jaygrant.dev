c91734bb36f3dc394a61b8fed965c6204e6fedaa
Jay Grant <dev@jaygrant.dev>
2024-07-19 23:46:49 -0400

    add complimentary/tetrad color generation

---
+++ js/base.js
@@ -0,0 +1,45 @@
+let colorPicker;
+const defaultColor = "#FB2AEB";
+
+window.addEventListener("load", startup, false);
+
+
+function startup() {
+    colorPicker = document.querySelector("#colorPicker");
+    colorPicker.value = defaultColor;
+    colorPicker.addEventListener("input", updateFirst, false);
+    colorPicker.addEventListener("change", updateAll, false);
+    colorPicker.select();
+  }
+  
+
+function switchColorTheme() {
+    let colorScheme = document.body.style.colorScheme;
+
+    if ((colorScheme == "") || (colorScheme == "light")) {
+        document.body.style.colorScheme = "dark";
+    } else {
+        document.body.style.colorScheme = "light";
+    }
+}
+
+
+function updateFirst(event) {
+    root = document.querySelector(':root');
+    rootStyle = getComputedStyle(document.querySelector(':root'));
+    root.style.setProperty('--primary-color', event.target.value);
+  }
+
+  function updateAll(event) {
+    root = document.querySelector(':root');
+    rootStyle = getComputedStyle(document.querySelector(':root'));
+    root.style.setProperty('--primary-color', event.target.value);
+  }
+  
+  
+
+function changePrimaryColor(input) {
+    root = document.querySelector(':root');
+    rootStyle = getComputedStyle(document.querySelector(':root'));
+
+}
