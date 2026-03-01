698ca76fcb7cc1029557c0b51c8d17f3a96c3654
Jay Grant <dev@jaygrant.dev>
2026-03-01 00:34:40 -0500

    two years later ... hrm

--- js/base.js
+++ js/base.js
@@ -1,45 +1,48 @@
 let colorPicker;
-const defaultColor = "#FB2AC7";    //color picker only tolerates hex, no I don't know why it keeps defaulting to a purple
+const defaultColor = "#FB2AC7";    //color picker only tolerates hex
 
 window.addEventListener("load", startup, false);
 
-
 function startup() {
-    colorPicker = document.querySelector("#colorPicker");
-    colorPicker.value = defaultColor;
-    colorPicker.addEventListener("input", updateFirst, false);
-    colorPicker.addEventListener("change", updateAll, false);
-    colorPicker.select();
-  }
-  
+		colorPicker = document.querySelector("#colorPicker");
+		colorPicker.value = defaultColor;
+		colorPicker.addEventListener("input", updateFirst, false);
+		colorPicker.addEventListener("change", updateAll, false);
+		colorPicker.select();
+
+		const canvas = document.querySelector("canvas");
+		const ctx = canvas.getContext("2d");
+		ctx.fillStyle = defaultColor;
+		ctx.fillRect(10, 10, 100, 100);
+	}
+	
 
 function switchColorTheme() {
-    let colorScheme = document.body.style.colorScheme;
+		let colorScheme = document.body.style.colorScheme;
 
-    if ((colorScheme == "") || (colorScheme == "light")) {
-        document.body.style.colorScheme = "dark";
-    } else {
-        document.body.style.colorScheme = "light";
-    }
+		if ((colorScheme == "") || (colorScheme == "light")) {
+				document.body.style.colorScheme = "dark";
+		} else {
+				document.body.style.colorScheme = "light";
+		}
 }
 
-
+/* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color */
 function updateFirst(event) {
-    root = document.querySelector(':root');
-    rootStyle = getComputedStyle(document.querySelector(':root'));
-    root.style.setProperty('--primary-color', event.target.value);
-  }
+		root = document.querySelector(':root');
+		rootStyle = getComputedStyle(document.querySelector(':root'));
+		root.style.setProperty('--primary-color', event.target.value);
+		document.querySelector("canvas").getContext('2d').fillStyle = event.target.value;
+	}
 
-  function updateAll(event) {
-    root = document.querySelector(':root');
-    rootStyle = getComputedStyle(document.querySelector(':root'));
-    root.style.setProperty('--primary-color', event.target.value);
-  }
-  
-  
-
+function updateAll(event) {
+	root = document.querySelector(':root');
+	rootStyle = getComputedStyle(document.querySelector(':root'));
+	root.style.setProperty('--primary-color', event.target.value);
+	document.querySelector("canvas").getContext('2d').fillStyle = event.target.value;
+}
+	
 function changePrimaryColor(input) {
-    root = document.querySelector(':root');
-    rootStyle = getComputedStyle(document.querySelector(':root'));
-
+		root = document.querySelector(':root');
+		rootStyle = getComputedStyle(document.querySelector(':root'));
 }
